9f7384d87e98a02e78e528d1ce5d830441a82a42
roy-hendry <royjairusraj@hotmail.com>
2024-08-17 21:06:35 +0100

    Created animated routes via layout

---
+++ src/lib/components/AnimatedRoute.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { page } from "$app/stores";
+	import { fly, fade } from "svelte/transition";
+</script>
+
+<!-- Key rebuilds all children onChange
+ in this case we use it to subscribe to the page URL -->
+{#key $page.url}
+	<div in:fade={{ duration: 500 }}>
+		<div in:fly={{ x: "-100%", duration: 750 }}>
+			<slot />
+		</div>
+	</div>
+{/key}
--- src/routes/login/+layout.svelte
+++ src/routes/login/+layout.svelte
@@ -1,17 +1,30 @@
 <script lang="ts">
-    import { page } from '$app/stores';
+	import { page } from "$app/stores";
+	import AnimatedRoute from "$lib/components/AnimatedRoute.svelte";
 </script>
 
 <nav class="flex justify-center my-6">
-    <ul class="steps steps-vertical lg:steps-horizontal">
-        <a href="/login" class="step step-primary">Sign in</a>
-        <a href="/login/username" class="step" class:step-primary={$page.route.id?.match(/username|photo/g)}>Choose Username</a>
-        <a href="/login/photo" class="step" class:step-primary={$page.route.id?.includes("photo")}>Upload photo</a>
-    </ul>
+	<ul class="steps steps-vertical lg:steps-horizontal">
+		<a href="/login" class="step step-primary">Sign in</a>
+		<a
+			href="/login/username"
+			class="step"
+			class:step-primary={$page.route.id?.match(/username|photo/g)}
+			>Choose Username</a
+		>
+		<a
+			href="/login/photo"
+			class="step"
+			class:step-primary={$page.route.id?.includes("photo")}
+			>Upload photo</a
+		>
+	</ul>
 </nav>
 
-<main class="card w-4/6 bg-neutral text-neutral-content mx-auto">
-    <div class="card-body items-center text-center">
-        <slot ></slot>
-    </div>
-</main>
+<AnimatedRoute>
+	<main class="card w-4/6 bg-neutral text-neutral-content mx-auto">
+		<div class="card-body items-center text-center">
+			<slot></slot>
+		</div>
+	</main>
+</AnimatedRoute>
