9631efc66a2854057218559f32aa9793fc4f7ef1
roy-hendry <royjairusraj@hotmail.com>
2024-08-18 20:06:42 +0100

    Auth Store implemented

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { writable } from "svelte/store";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyDcapYKNVis3qyP6_wl0NIJMzLAehrRcOI",
@@ -17,3 +18,29 @@ export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
+
+function userStore() {
+	let unsubscribe: () => void;
+
+	if (!auth || !globalThis.window) {
+		console.warn("Auth is not initialized or not in browser");
+		const { subscribe } = writable<User | null>(null);
+		return {
+			subscribe,
+		};
+	}
+
+	const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+		unsubscribe = onAuthStateChanged(auth, (user) => {
+			set(user);
+		});
+
+		return () => unsubscribe();
+	});
+
+	return {
+		subscribe,
+	};
+}
+
+export const user = userStore();
