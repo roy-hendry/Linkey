731b6fdd3f7beaecd76bbd6d956f67fe4d8f392a
roy-hendry <royjairusraj@hotmail.com>
2024-08-20 21:30:04 +0100

    Implemented custom usernames and db check for existing usernames

--- src/lib/components/AuthCheck.svelte
+++ src/lib/components/AuthCheck.svelte
@@ -5,8 +5,6 @@
 {#if $user}
 	<slot />
 {:else}
-	<p class="text-error my-10">
-		You must be signed in to view this page
-		<a class="btn btn-primary" href="/login">Sign in</a>
-	</p>
+	<p class="text-error my-5">You must be signed in to view this page</p>
+	<a class="btn btn-primary" href="/login">Sign in</a>
 {/if}
--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -1,5 +1,51 @@
 <script lang="ts">
+	import AuthCheck from "$lib/components/AuthCheck.svelte";
+	import { db, user } from "$lib/firebase";
+	import { doc, getDoc, writeBatch } from "firebase/firestore";
 
+	let username = "";
+	let loading = false;
+	let isAvailable = false;
+
+	let debountTimer: NodeJS.Timeout;
+
+	async function checkAvailability() {
+		isAvailable = false;
+		clearTimeout(debountTimer);
+
+		loading = true;
+
+		// This is a debouncing technique to prevent the function from being called too frequently
+		// It will only call after the user has stopped typing for 500ms
+		debountTimer = setTimeout(async () => {
+			console.log("Checking availability of", username);
+
+			// Check if the username already exists in the database
+			const ref = doc(db, "usernames", username);
+			// If the document exists, then the username is not available
+			const exists = await getDoc(ref).then((doc) => doc.exists());
+
+			isAvailable = !exists;
+			loading = false;
+		}, 500);
+	}
+
+	async function confirmUsername() {}
 </script>
 
-<h2>Username</h2>
+<AuthCheck>
+	<h2>Username</h2>
+
+	<form class="w-2/5" on:submit|preventDefault={confirmUsername}>
+		<input
+			type="text"
+			placeholder="Username"
+			class="input w-full"
+			bind:value={username}
+			on:input={checkAvailability}
+		/>
+		<p>{isAvailable ? "Available" : "Not available"}</p>
+
+		<button class="btn btn-success">Confirm username @{username}</button>
+	</form>
+</AuthCheck>
