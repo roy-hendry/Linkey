49220699a01aefc57c764df3f9dea47e11e435ee
roy-hendry <royjairusraj@hotmail.com>
2024-09-01 20:16:20 +0100

    Cookie based auth implemented

---
+++ src/routes/api/signin/+server.ts
@@ -0,0 +1,42 @@
+import type { RequestHandler } from "./$types";
+import { adminAuth } from "$lib/server/admin";
+import { error, json } from "@sveltejs/kit";
+
+// FYI cookies are values that are defined on the server and sent to the browser and stored client-side
+
+// User for cookie based auth
+export const POST: RequestHandler = async ({ request, cookies }) => {
+  // Pass ID token to server
+  const { idToken } = await request.json();
+
+  const expiresIn = 60 * 60 * 24 * 5 * 1000; // 5 days
+
+  // Decode the ID token securely on the Firebase server
+  const decodedIdToken = await adminAuth.verifyIdToken(idToken);
+
+  // Check if user signed in within the last 5 minutes
+  if (new Date().getTime() / 1000 - decodedIdToken.auth_time < 5 * 60) {
+    // Create session cookie
+    const cookie = await adminAuth.createSessionCookie(idToken, { expiresIn });
+
+    // Set session cookie
+    const options = {
+      maxAge: expiresIn,
+      httpOnly: true,
+      secure: true,
+      path: "/",
+    };
+
+    // The cookie needs to be called "__session" for Firebase to recognise it
+    cookies.set("__session", cookie, options);
+
+    return json({ status: "signedIn" });
+  } else {
+    throw error(401, "Recent sign in required!");
+  }
+};
+
+export const DELETE: RequestHandler = async ({ cookies }) => {
+  cookies.delete("__session", { path: "/" });
+  return json({ status: "signedOut" });
+};
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,28 +1,38 @@
 <script lang="ts">
-	import { auth, user } from "$lib/firebase";
-	import {
-		GoogleAuthProvider,
-		signInWithPopup,
-		signOut,
-	} from "firebase/auth";
+  import { auth, user } from "$lib/firebase";
+  import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
-	async function signInWithGoogle() {
-		const provider = new GoogleAuthProvider();
-		const user = await signInWithPopup(auth, provider);
-		console.log(user);
-	}
+  async function signInWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    const credential = await signInWithPopup(auth, provider);
+
+    const idToken = await credential.user.getIdToken();
+
+    const res = await fetch("/api/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        // Usually we'd need to specify the CSRF token here but it is handled by SvelteKit automatically
+        // "CSRF-Token": csrfToken
+      },
+      body: JSON.stringify({ idToken }),
+    });
+  }
+
+  async function signOutSSR() {
+    const res = await fetch("/api/signin", { method: "DELETE" });
+    await signOut(auth);
+  }
 </script>
 
 <h2>Login</h2>
 
 {#if $user}
-	<h2 class="card-title">Welcome, {$user.displayName}</h2>
-	<p class="text-center text-success">You are logged in</p>
-	<button class="btn btn-warning" on:click={() => signOut(auth)}
-		>Sign out</button
-	>
+  <h2 class="card-title">Welcome, {$user.displayName}</h2>
+  <p class="text-center text-success">You are logged in</p>
+  <button class="btn btn-warning" on:click={signOutSSR}>Sign out</button>
 {:else}
-	<button class="btn btn-primary" on:click={signInWithGoogle}
-		>Sign in with Google</button
-	>
+  <button class="btn btn-primary" on:click={signInWithGoogle}
+    >Sign in with Google</button
+  >
 {/if}
