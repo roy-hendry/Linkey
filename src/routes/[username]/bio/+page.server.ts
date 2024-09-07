868c328205f86b0847d3224bdd3179291ee3e290
roy-hendry <royjairusraj@hotmail.com>
2024-09-07 09:53:24 +0100

    Actions to submit the user bio

--- src/routes/[username]/bio/+page.server.ts
+++ src/routes/[username]/bio/+page.server.ts
@@ -1,10 +1,12 @@
-import type { PageServerLoad } from "./$types";
+import type { PageServerLoad, Actions } from "./$types";
 import { adminAuth, adminDB } from "$lib/server/admin";
-import { error, redirect } from "@sveltejs/kit";
+import { error, fail, redirect } from "@sveltejs/kit";
 
 export const load = (async ({ locals, params }) => {
   // Very easy to access the userId
   const uid = locals.userId;
+
+  console.log("uid", uid);
 
   if (!uid) {
     throw redirect(301, "/login");
@@ -24,3 +26,29 @@ export const load = (async ({ locals, params }) => {
     bio,
   };
 }) satisfies PageServerLoad;
+
+export const actions = {
+  // As we only have one action we can just use the default export
+  default: async ({ locals, request, params }) => {
+    const uid = locals.userId;
+
+    const data = await request.formData();
+    const bio = data.get("bio");
+
+    const userRef = adminDB.collection("users").doc(uid!);
+    const { username } = (await userRef.get()).data()!;
+
+    if (params.username !== username) {
+      throw error(401, "That username does not belong to you");
+    }
+
+    if (bio!.length > 260) {
+      // We can use the fail function to return a 400 error with a JSON body
+      // The fail function won't render the error page, it will just pass that data client side
+      throw fail(400, { problem: "Bio must be less than 260 characters" });
+    }
+
+    // Update bio field in the user document
+    await userRef.update({ bio });
+  },
+} satisfies Actions;
--- src/routes/[username]/bio/+page.svelte
+++ src/routes/[username]/bio/+page.svelte
@@ -1,7 +1,31 @@
 <script lang="ts">
   import type { PageData } from "./$types";
+  import { page } from "$app/stores";
+  import { enhance } from "$app/forms";
 
   export let data: PageData;
 </script>
 
-<h1>Edit Bio</h1>
+<main class="max-w-lg prose text-center mx-auto my-6">
+  <p>Current Bio: <span class="text-info">{data.bio}</span></p>
+
+  <!-- Using stores to display status code and form errors -->
+  <p>Status Code: {$page.status}</p>
+  <!-- We check for problems which we set on the page.server.ts file if the length is too high -->
+  <p class="text-error">{$page.form?.problem ?? ""}</p>
+
+  <!-- This is a POST instead of an onSubmit event handler as we want to send this to the server to be handled by a SvelteKit action -->
+  <form method="POST" use:enhance>
+    <div class="form-control">
+      <label for="bio" class="label">
+        <span class="label-text">Your bio</span>
+      </label>
+      <textarea
+        name="bio"
+        class="textarea textarea-bordered textarea-accent"
+        value={data.bio}
+      />
+    </div>
+    <button class="btn btn-primary my-5">Update Bio</button>
+  </form>
+</main>
